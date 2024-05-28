4c3dd6b43efd63ea593c9e92bb1aded64a379c00
Delario4994 <Delarionancejr@gmail.com>
2024-05-28 03:27:25 -0400

    cleaned up index HTML and CSS

--- app/backend/routes/routes.mjs
+++ app/backend/routes/routes.mjs
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'url';
 import path, {dirname} from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const dirHTMLFiles = path.join(__dirname,  "../frontend/")
+const dirHTMLFiles = path.join(__dirname,  "..", "..", "/frontend/")
 import express from 'express';
 const router = express.Router();
 
