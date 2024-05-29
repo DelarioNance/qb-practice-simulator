725a8ff9af3facab39f2eda143e4d954e4f68882
Delario4994 <Delarionancejr@gmail.com>
2024-05-29 14:39:16 -0400

    edited index.js

--- app/frontend/scripts/index.js
+++ app/frontend/scripts/index.js
@@ -3,18 +3,19 @@
  * teams and sends the user to the page of the newly created match.
  */
 async function startMatch() {
-    const home = document.getElementById("homeID").value
-    const away = document.getElementById("awayID").value
+    const home = document.getElementById("homeID").value;
+    const away = document.getElementById("awayID").value;
 
     // Create a new match in the database
-    const response = await fetch(`/match/?homeID=${home}&awayID=${away}`, {
+    const responsePromise = await fetch(`/match/?homeID=${home}&awayID=${away}`, {
         method: "POST"
     });
-    const idOfNewMatch = await response.json().insertId;
+    const insertedMatchId = await responsePromise.json().insertId;
+    console.log(insertedMatchId);
 
     // window.location.assign is used instead of HTML form since the 
     // user is being sent to an HTML template rather than a static 
     // HTML page. If an HTML form was used instead, then it would be 
     // more difficult to pass data into the match template.
-    window.location.assign(`/match?matchID=${idOfNewMatch}`)
+    window.location.assign(`/match?matchID=${insertedMatchId}`);
 }
